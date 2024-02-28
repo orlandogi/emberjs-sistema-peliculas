f201513726c9b2879470ff0fc02811f49362354f
Orlando Galvan Vargas <159957306+OrlandoGalvanVargas@users.noreply.github.com>
2024-02-28 16:15:51 -0600

    Estructura de carpetas

---
+++ ember-cli-build.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      bootstrapVersion: 5,
+      importBootstrapCSS: true
+    }
+  });
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
+
+  return app.toTree();
+};
+
