8db39a0b53543906e09e767553746a916cf5665f
Orlando Galvan Vargas <159957306+OrlandoGalvanVargas@users.noreply.github.com>
2024-03-15 00:40:10 -0600

    Boton eliminar funcionando v2

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,13 +1,20 @@
 import Component from '@glimmer/component';
 import axios from 'axios';
+import Swal from 'sweetalert2'
 
 export default class PeopleListComponent extends Component {
   async deleteUser(user) {
     try {
-      await axios.delete(`http://localhost:3000/api/usuarios/${user}`);
-      console.log('Usuario eliminado correctamente');
+      await (
+        axios.delete(`http://localhost:3000/api/usuario/${user}`),
+        Swal.fire("Eliminado!", "", "success")
+     )
+     location.reload();
     } catch (error) {
-      console.error('Error al eliminar usuario:', error);
-    }
+      await Swal.fire({
+        icon: "error",
+        title: "Algo salio mal!",
+        text: "No se elimino"
+      });    }
   }
 }
